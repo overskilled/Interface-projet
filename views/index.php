a3a0695ef76ef2c11ba5e703b53f117858e824c3
overskilled237 <ouateedemloic@gmail.com>
2022-12-29 03:23:20 +0100

    still empty

---
+++ views/index.php
@@ -0,0 +1,17 @@
+<?php
+  include_once 'Header.php';
+  include_once '..\includes\db.inc.php';
+?>
+ 
+ 
+
+
+
+ 
+  <!--
+    <div class="container">
+      <input type="button" onclick="window.location.href='Client.php';" value="Ajouter un client" /><br><br>
+      <input type="button" onclick="window.location.href='Fourniseur.php';" value="Ajouter un fournisseur" /><br><br>
+      <input type="button" onclick="window.location.href='article.php';" value="Ajouter un article au magasin" /><br><br>
+      <input type="button" onclick="window.location.href='commande.php';" value="Commander un article" />
+    </div>
