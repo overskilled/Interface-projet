ae56d5e4c3e07eeffb9811304a5453525f76c73b
overskilled237 <ouateedemloic@gmail.com>
2022-12-29 23:11:02 +0100

    Main page

--- views/index.php
+++ views/index.php
@@ -1,17 +1,10 @@
 <?php
   include_once 'Header.php';
   include_once '..\includes\db.inc.php';
+ 
+  if (isset($_SESSION['Id'])) {
+    echo "<p>Welcome ".$_SESSION['name']."</p>";
+  }
+
 ?>
  
- 
-
-
-
- 
-  <!--
-    <div class="container">
-      <input type="button" onclick="window.location.href='Client.php';" value="Ajouter un client" /><br><br>
-      <input type="button" onclick="window.location.href='Fourniseur.php';" value="Ajouter un fournisseur" /><br><br>
-      <input type="button" onclick="window.location.href='article.php';" value="Ajouter un article au magasin" /><br><br>
-      <input type="button" onclick="window.location.href='commande.php';" value="Commander un article" />
-    </div>
