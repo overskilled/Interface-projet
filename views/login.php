7479a0935ee0719111a8d2d1d57e1ab7da847ad7
overskilled237 <ouateedemloic@gmail.com>
2022-12-29 03:23:51 +0100

    Login form page

---
+++ views/login.php
@@ -0,0 +1,18 @@
+<?php
+    include_once 'Header.php';
+    include_once '..\includes\db.inc.php';
+
+?>
+
+
+<div class="container my-5">
+    <div class="form-group">
+        <label for="exampleInputEmail1"><b>Email address</b></label>
+        <input type="email" class="form-control" id="exampleInputEmail1" name="email" placeholder="Enter your email">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1"><b>Password</b></label>
+        <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Enter your password">
+    </div>
+
+</div>
